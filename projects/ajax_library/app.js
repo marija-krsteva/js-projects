8e904cfa0fc3ac12006cb14baba9f1a3a8c2e484
marija.krsteva <marija.krsteva>
2020-10-30 18:46:27 +0100

    EasyHTTP project added

---
+++ projects/ajax_library/app.js
@@ -0,0 +1,53 @@
+const http = new easyHTTP();
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+// Get Single Posts
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
+
+// Create Data
+
+const data = {
+    title: 'Custom Post',
+    body: "This is a custom post"
+};
+
+// Create post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
+
+// Update post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(response);
+    }
+});
